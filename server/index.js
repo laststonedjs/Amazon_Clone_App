c5d4dced0f3a51c315fecaa1c7ca25d978878a51
Filip <filipsarovic428@gmail.com>
2022-11-29 18:44:02 +0100

    Created SignUp functionality for User

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
+// import from packages
 const express = require('express');
+const mongoose = require('mongoose');
+// import other files(services)
+const authRouter = require('./routes/auth');
+// init
 const PORT = 3000;
-
 const app = express();
+const DB = 'mongodb+srv://lastoned:Albertobalsam101@cluster0.4mrgmvb.mongodb.net/?retryWrites=true&w=majority';
 
-app.get('/', (req, res) => {
-  res.json({ name: 'Filip' });
+// middleware (client -> middleware -> server -> client)
+app.use(express.json());
+app.use(authRouter);
+
+// connections
+mongoose.connect(DB).then(() => { // we pass 'then()' here because this is a promise function
+  console.log("Connection successful");
+}).catch((e) => {
+  console.log(e);
 })
-
-app.get('/green-bank', (req, res) => {
-  res.send('green bank!');
-});
-
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Connected at PORT ${PORT}`);
