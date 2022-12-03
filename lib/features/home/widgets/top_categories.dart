84311b2990621ef9f818de2daf400f61ec770eaf
Filip <filipsarovic428@gmail.com>
2022-12-03 09:16:46 +0100

    Built UI for Home Page

---
+++ lib/features/home/widgets/top_categories.dart
@@ -0,0 +1,43 @@
+import 'package:amazon_clone_tech387/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class TopCategories extends StatelessWidget {
+  const TopCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+        itemCount: GlobalVariables.categoryImages.length,
+        scrollDirection: Axis.horizontal,
+        itemExtent: 75,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset(
+                    GlobalVariables.categoryImages[index]['image']!,
+                    fit: BoxFit.cover,
+                    height: 45,
+                    width: 45,
+                  ),
+                ),
+              ),
+              Text(
+                GlobalVariables.categoryImages[index]['title']!,
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
