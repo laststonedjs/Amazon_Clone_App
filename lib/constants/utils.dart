d956a3cff8ac149b41d0bf7668d123c9625e4ab0
Filip <filipsarovic428@gmail.com>
2022-12-03 12:32:07 +0100

    Functionality for picking images from device

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,3 +1,10 @@
+// This library allows you to work with files, directories, sockets, processes,
+// HTTP servers and clients, and more.
+// Many operations related to input and output are asynchronous
+// and are handled using [Future]s or [Stream]s
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
@@ -6,4 +13,24 @@ void showSnackBar(BuildContext context, String text) {
       content: Text(text),
     ),
   );
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  try {
+    var files = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+    // here we check if files what are we picked not null and notEmpty,
+    // loop over files and added in List<File> images array.
+    if (files != null && files.files.isNotEmpty) {
+      for (int i = 0; i < files.files.length; i++) {
+        images.add(File(files.files[i].path!));
+      }
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
 }
--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:amazon_clone_tech387/common/widgets/custom_button.dart';
 import 'package:amazon_clone_tech387/common/widgets/custom_textfield.dart';
+import 'package:amazon_clone_tech387/constants/utils.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -34,6 +38,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
    */
 
   String category = 'Mobiles';
+  List<File> images = [];
 
   @override
   void dispose() {
@@ -51,6 +56,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion'
   ];
+
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,36 +90,56 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 4],
-                  strokeCap: StrokeCap.round,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.folder_open_outlined,
-                          size: 40,
+                images.isNotEmpty
+                    ? CarouselSlider(
+                        items: images.map((i) {
+                          return Builder(
+                            builder: (BuildContext context) => Image.file(
+                              i,
+                              fit: BoxFit.cover,
+                              height: 200,
+                            ),
+                          );
+                        }).toList(), // 'items' will return the list of widgets and we use 'carousel images' list from GlobalVariables.dart
+                        options: CarouselOptions(
+                          viewportFraction: 1,
+                          height: 200,
                         ),
-                        const SizedBox(
-                          height: 15,
+                      )
+                    : GestureDetector(
+                        onTap: selectImages,
+                        child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          strokeCap: StrokeCap.round,
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.folder_open_outlined,
+                                  size: 40,
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  'Select Product Images',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.shade400),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        Text(
-                          'Select Product Images',
-                          style: TextStyle(
-                              fontSize: 15, color: Colors.grey.shade400),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                      ),
                 const SizedBox(
                   height: 30,
                 ),
