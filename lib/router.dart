e0299d6ff6102889227253bfcd44283b9081d7ff
Filip <filipsarovic428@gmail.com>
2022-12-03 11:07:13 +0100

    Admin Screen UI layout

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone_tech387/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone_tech387/features/admin/screens/add_product_screen.dart';
+import 'package:amazon_clone_tech387/features/admin/screens/posts_screen.dart';
 import 'package:amazon_clone_tech387/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone_tech387/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +22,11 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const BottomBar(),
+      );
+    case AddProductScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AddProductScreen(),
       );
     default:
       return MaterialPageRoute(
